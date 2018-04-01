8c92e889b724afb1367cf56bfd7ef5f13b4afb8c
paul <ztt1995120@gmail.com>
2018-04-01 21:21:55 +0800

    * 4. 最长子字符串的获得，by 简单的数组方式

--- app/src/main/java/com/example/thinkpad/t47checklisthead/MyJavaTest.java
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/MyJavaTest.java
@@ -11,39 +11,49 @@ import java.util.List;
  * 1. show u how to get method generic return type.
  * 2. show u how to get method generic parameter type.
  * 3. mk interview cheat
+ * 4. 最长子字符串的获得，by 简单的数组方式
  */
 
 public class MyJavaTest {
     public static void main(String[] args) throws NoSuchMethodException {
-//        System.out.print(foo(17, 8));
-        int a,b,c;
-        a=b=c=5;
-        a=++b-(++c);//0,6,6
-        System.out.println(a+","+b+","+c);
-        a=++b+(++c);//
-        System.out.println(a+","+b+","+c);
-        a = (b--) + (c--);
-        System.out.println(a+","+b+","+c);
-        a = (b--);//tt: u will got a=6, but b will be 5 at end
-        System.out.println(a+"");
+        System.out.print(findGreatest(new int[]{1, -2, 3, 10, -4, 7, 2, -5}));
 
     }
 
-    public static int foo(int x, int y) {
-        if (x <= 0 || y <= 0) {
-            return 1;
+    private static int findGreatest(int[] scoreArr) {
+        //boundary check
+
+        int curSum = 0;
+        int greateestSum = 0;
+        for (int i = 0; i < scoreArr.length; i++) {
+            if (curSum <= 0) {
+                curSum = scoreArr[i];
+            } else {
+                curSum += scoreArr[i];
+            }
+            if (curSum > greateestSum) {
+                greateestSum = curSum;
+            }
         }
-        else {
-            return 3 * foo(x - 6, y / 2);
+        return greateestSum;
+    }
+
+    /**
+     * unfinish, no good thoughts
+     * */
+    private static void func(int startP, String s, int desire) {
+        int counter = 0;
+        for (int i = startP; i < s.length() - startP; i ++) {
+            if (s.charAt(startP) == s.charAt(++startP)) {
+                counter ++;
+                if (counter == desire) {
+                    if (s.charAt(startP) != s.charAt(startP + 1)) {
+                        System.out.println();
+                    }
+                }
+            }
         }
     }
 
-    class MyClass {
 
-        protected List<String> stringList = new ArrayList<>();
-
-        public void getStringList(List<Integer> param) {
-            return;
-        }
-    }
 }
