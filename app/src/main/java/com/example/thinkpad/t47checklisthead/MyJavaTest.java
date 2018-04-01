bc0e785efb420e73308ea98cb7890a2d7fb0b0a6
paul <ztt1995120@gmail.com>
2018-04-01 20:32:29 +0800

    * 3. mk interview cheat

--- app/src/main/java/com/example/thinkpad/t47checklisthead/MyJavaTest.java
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/MyJavaTest.java
@@ -10,27 +10,32 @@ import java.util.List;
  * Created by paul on 23/03/2018.
  * 1. show u how to get method generic return type.
  * 2. show u how to get method generic parameter type.
+ * 3. mk interview cheat
  */
 
 public class MyJavaTest {
     public static void main(String[] args) throws NoSuchMethodException {
-        Method method = MyClass.class.getMethod("getStringList", List.class);
-        //we want the generic param type contained in this method.
-        Type [] genericParameterTypes = method.getGenericParameterTypes();
+//        System.out.print(foo(17, 8));
+        int a,b,c;
+        a=b=c=5;
+        a=++b-(++c);//0,6,6
+        System.out.println(a+","+b+","+c);
+        a=++b+(++c);//
+        System.out.println(a+","+b+","+c);
+        a = (b--) + (c--);
+        System.out.println(a+","+b+","+c);
+        a = (b--);//tt: u will got a=6, but b will be 5 at end
+        System.out.println(a+"");
 
-        for (Type genericParameterType : genericParameterTypes) {
-            if (genericParameterType instanceof ParameterizedType) {
-                ParameterizedType aType = (ParameterizedType) genericParameterType;
-                Type [] parameterArgTypes = aType.getActualTypeArguments();
-                for (Type parameterArgType : parameterArgTypes) {
-                    Class parameterArgClass = (Class) parameterArgType;
-                    System.out.print(parameterArgClass);
+    }
 
-                }
-            }
+    public static int foo(int x, int y) {
+        if (x <= 0 || y <= 0) {
+            return 1;
         }
-
-
+        else {
+            return 3 * foo(x - 6, y / 2);
+        }
     }
 
     class MyClass {
