368234531a640500136e164827be0847de5371aa
paul <ztt1995120@gmail.com>
2018-04-23 17:30:25 +0800

    add fragment and xml, gradle files

---
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/fragment/ContentFragment.java
@@ -0,0 +1,104 @@
+package com.example.thinkpad.t47checklisthead.fragment;
+
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.thinkpad.t47checklisthead.R;
+
+import yalantis.com.sidemenu.interfaces.ScreenShotable;
+
+/**
+ * Created by paul on 2018/4/23.
+ */
+
+public class ContentFragment extends android.support.v4.app.Fragment implements ScreenShotable {
+    public static final String CLOSE = "Close";
+    public static final String BUILDING = "Building";
+    public static final String BOOK = "Book";
+    public static final String PAINT = "Paint";
+    public static final String CASE = "Case";
+    public static final String SHOP = "Shop";
+    public static final String PARTY = "Party";
+    public static final String MOVIE = "Movie";
+
+    private View containerView;
+    protected ImageView mImageView;
+    protected int res;
+    private Bitmap bitmap;
+
+    public static ContentFragment newInstance(int resId) {
+        ContentFragment contentFragment = new ContentFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(Integer.class.getName(), resId);
+        contentFragment.setArguments(bundle);
+        return contentFragment;
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        res = getArguments().getInt(Integer.class.getName());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_walk, container, false);
+        mImageView = (ImageView) rootView.findViewById(R.id.image_content);
+        mImageView.setClickable(true);
+        mImageView.setFocusable(true);
+        mImageView.setImageResource(res);
+        return rootView;
+    }
+
+    /**
+     * Called immediately after {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}
+     * has returned, but before any saved state has been restored in to the view.
+     * This gives subclasses a chance to initialize themselves once
+     * they know their view hierarchy has been completely created.  The fragment's
+     * view hierarchy is not however attached to its parent at this point.
+     * @param view The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     */
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.containerView = view.findViewById(R.id.container);
+    }
+
+    //tt: why it mean? screenshot
+    //tt: guess it will save the bitmap for animation
+    @Override
+    public void takeScreenShot() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Bitmap bitmap = Bitmap.createBitmap(containerView.getWidth(),
+                        containerView.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                containerView.draw(canvas);
+                ContentFragment.this.bitmap = bitmap;
+            }
+        };
+
+        thread.start();
+
+    }
+
+    @Override
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+}
+
+
