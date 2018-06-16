110baa20b38652111f2ca5c80f3a08c1f100439c
paul <ztt1995120@gmail.com>
2018-06-16 13:25:31 +0800

    ### 19.0 测试通过viewpager底层的，一种app介绍页面 demo

--- app/src/main/java/com/example/thinkpad/t47checklisthead/MainActivity.java
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/MainActivity.java
@@ -1,30 +1,26 @@
 package com.example.thinkpad.t47checklisthead;
 
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
 
+    private ViewPager mViewPager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-    }
 
-    public void sendBroadcast(View view) {
-        Intent intent = new Intent("com.example.thinkpad.t47checklisthead.CUSTOM_INTENT");
-        sendBroadcast(intent);
-        Log.d(TAG, "sendBroadcast: sent!!"+ intent.getAction());
-    }
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
 
+        // Set an Adapter on the ViewPager
+        mViewPager.setAdapter(new IntroAdapter(getSupportFragmentManager()));
+
+        // Set a PageTransformer
+        mViewPager.setPageTransformer(false, new IntroPageTransformer());
+    }
 }
