7a042d877038ab09e267e8ca8685fd0ed50cd8e3
paul <ztt1995120@gmail.com>
2018-06-16 12:36:20 +0800

    ### 18.6 broadcast receiver, 学习youtube仍然没反应

--- app/src/main/java/com/example/thinkpad/t47checklisthead/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/MyBroadcastReceiver.java
@@ -17,15 +17,8 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     String ACTION = "github.nisrulz.action.MY_ACTION";
 
 
-    public MyBroadcastReceiver() {
-    }
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "onReceive:!! ");
-        if (intent != null && intent.getAction().equals(ACTION)) {
-            String data = intent.getStringExtra("data");
-            Toast.makeText(context, data, Toast.LENGTH_SHORT).show();
-        }
+
     }
 }
