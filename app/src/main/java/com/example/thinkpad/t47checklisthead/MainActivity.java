ce256509a53cb2407f22e008e688c0ced143dcdf
paul <ztt1995120@gmail.com>
2018-09-15 11:05:24 +0800

    ### 20.0 阅读绘制圆角矩形的源码，看 drawBitmap(),  RectF, PorterDuffXfermode

--- app/src/main/java/com/example/thinkpad/t47checklisthead/MainActivity.java
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.thinkpad.t47checklisthead;
 
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -15,12 +22,25 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        // Set an Adapter on the ViewPager
-        mViewPager.setAdapter(new IntroAdapter(getSupportFragmentManager()));
+    }
 
-        // Set a PageTransformer
-        mViewPager.setPageTransformer(false, new IntroPageTransformer());
+    public static Bitmap makeRoundCorner(Bitmap bitmap, int px) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        Bitmap output = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        int color = 0xff424242;
+        Paint paint = new Paint();
+        Rect rect = new Rect(0, 0, width, height);
+        RectF rectF = new RectF(rect);
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, px, px, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
     }
 }
