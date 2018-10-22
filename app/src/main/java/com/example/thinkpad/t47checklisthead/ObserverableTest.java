5327a02ebcf77f534e71c2c7dadc9da938a6b3ce
paul <ztt1995120@gmail.com>
2018-10-23 07:37:57 +0800

    追加提交观察者的模式的被观察者

---
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/ObserverableTest.java
@@ -0,0 +1,12 @@
+package com.example.thinkpad.t47checklisthead;
+
+import java.util.Observable;
+
+/**
+ * Created by paul on 2018/10/21
+ * last modified at 21:11.
+ * Desc:
+ */
+
+public class ObserverableTest extends Observable{
+}
