1ccb75c3245b42516e2afbcaca1c884be203b5df
paul <ztt1995120@gmail.com>
2018-11-25 22:51:39 +0800

    0.4 create，基础使用
    不清理，才能完成tt1

--- app/src/main/java/com/example/thinkpad/t47checklisthead/rx/RxDemoActivity.java
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/rx/RxDemoActivity.java
@@ -20,8 +20,12 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
@@ -50,6 +54,7 @@ public class RxDemoActivity extends AppCompatActivity {
     ApiService apiService;
     TextView tv_text;
     EditText editText;
+    StringBuilder mRxOperatorsText;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,16 +62,63 @@ public class RxDemoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rx);
         tv_text = findViewById(R.id.tv_text);
         editText = findViewById(R.id.editText);
+        mRxOperatorsText = new StringBuilder();
 
-        Observable.just(1, 2, 3, 4)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Integer>() {
-                    @Override
-                    public void accept(Integer integer) throws Exception {
-                        Log.d(TAG, "accept: number: " + integer);
-                    }
-                });
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                // event 1
+                mRxOperatorsText.append("Observable emit 1" + "\n");
+                Log.e(TAG, "Observable emit 1" + "\n");
+                e.onNext(1);
+                // 2
+                mRxOperatorsText.append("Observable emit 2" + "\n");
+                Log.e(TAG, "Observable emit 2" + "\n");
+                e.onNext(2);
+                // ev 3
+                mRxOperatorsText.append("Observable emit 3" + "\n");
+                Log.e(TAG, "Observable emit 3" + "\n");
+                e.onNext(3);
+                // complete event
+                e.onComplete();
+                // fake ev 4
+                mRxOperatorsText.append("Observable emit 4" + "\n");
+                Log.e(TAG, "Observable emit 4" + "\n");
+                e.onNext(4);
+            }
+        }).subscribe(new Observer<Integer>() {
+            private int i;
+            private Disposable mDisposable;
+
+            public void onSubscribe(@NonNull Disposable d) {
+                mRxOperatorsText.append("onSubscribe: " + d.isDisposed() + "\n");
+                Log.e(TAG, "onSubscribe: " + d.isDisposed() + "\n");
+                mDisposable = d;
+            }
+
+            public void onNext(@NonNull Integer integer) {
+                mRxOperatorsText.append("onNext : value : " + integer + "\n");
+                Log.e(TAG, "onNext : value: " + integer + "\n");
+                i++;
+                // tt 以下的if 没有注释， 则不会执行到 tt1
+                if (i == 2) {
+                    // clear dispose(like event sequence), so the lower reachers can not receive the upstream events anymore
+                    mDisposable.dispose();
+                    mRxOperatorsText.append("onNext: isDisposed : " + mDisposable.isDisposed() + "\n");
+                    Log.e(TAG, "onNext: isDisposed: " + mDisposable.isDisposed() + "\n");
+                }
+            }
+            public void onError(@NonNull Throwable e) {
+                mRxOperatorsText.append("onError: value:" + e.getMessage() + "\n");
+                Log.e(TAG, "onError : value: " + e.getMessage() + "\n");
+            }
+            // tt1
+            public void onComplete() {
+                mRxOperatorsText.append("onComplete" + " \n");
+                tv_text.setText(mRxOperatorsText);
+                Log.e(TAG, "onComplete" + "\n");
+            }
+        });
     }
 
 }
