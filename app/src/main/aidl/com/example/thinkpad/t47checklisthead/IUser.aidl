277f743f9c8977e6ea0bb71c083227ef6fb1ead7
paul <ztt1995120@gmail.com>
2019-02-24 21:32:50 +0800

    1 清理ok 新主线，开始复习IPC artC2

--- app/src/main/aidl/com/example/thinkpad/t47checklisthead/IUser.aidl
+++
@@ -1,9 +0,0 @@
-// IUser.aidl
-package com.example.thinkpad.t47checklisthead;
-
-// Declare any non-default types here with import statements
-
-interface IUser {
-   boolean login(String userName,String userPwd);
-   void logout(String userName);
-}
--- app/src/main/java/com/example/thinkpad/t47checklisthead/MainActivity.java
+++ app/src/main/java/com/example/thinkpad/t47checklisthead/MainActivity.java
@@ -34,23 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: !!!");
-        final ThreadLocal<Boolean> mBooleanThreadLocal = new ThreadLocal<>();
-        mBooleanThreadLocal.set(true);
-        Log.d(TAG, "thread main > mBooleanThreadLocal = " + mBooleanThreadLocal.get());
-        new Thread("Thread#1"){
-            @Override
-            public void run() {
-                mBooleanThreadLocal.set(false);
-                Log.d(TAG, "run: thread 1 > mBooleanThreadLocal = " + mBooleanThreadLocal.get());
-            }
-        }.start();
-        new Thread("Thread#2"){
-            @Override
-            public void run() {
-                mBooleanThreadLocal.set(false);
-                Log.d(TAG, "run: thread 2 > mBooleanThreadLocal = " + mBooleanThreadLocal.get());
-            }
-        }.start();
+
     }
 
     @Override
